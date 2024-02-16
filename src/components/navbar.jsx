65150d22eac3afa039ff069d39b9d23e3697d717
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-16 20:57:12 +0900

    :sparkles: feat: add menu lists

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,12 @@
 import Link from 'next/link';
 
+const links = [
+  { url: '/', title: 'Home' },
+  { url: '/about', title: 'About' },
+  { url: '/portfolio', title: 'Portfolio' },
+  { url: '/contact', title: 'Contact' },
+];
+
 const Navbar = () => {
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
@@ -18,6 +25,14 @@ const Navbar = () => {
           <div className='w-10 h-1 bg-black rounded'></div>
           <div className='w-10 h-1 bg-black rounded'></div>
         </button>
+        {/* MENU LIST */}
+        <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+          {links.map((link) => (
+            <Link href={link.url} key={link.title}>
+              {link.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
